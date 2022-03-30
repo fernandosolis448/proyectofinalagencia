7907f167ba56e9e2176df2d9c58add1951aec823
fernandosolis448 <luis.solis.is@unipolidgo.edu.mx>
2022-03-29 23:17:55 -0600

    Update autoValidator.js

--- validations/autoValidator.js
+++ validations/autoValidator.js
@@ -1,10 +1,10 @@
 const { check, validationResult } = require('express-validator');
 
 const generateautoValidators = () => [
-    check('alias').notEmpty().isLength({max:50}).withMessage("Invalid alias"),
-    check('type').notEmpty().isIn(['DOG','CAT']).withMessage("Invalid pet"),
-    check('color').notEmpty().isLength({max:50}).withMessage("Invalid color"),
-    check('notes').notEmpty().isLength({max:150}).withMessage("Invalid notes"),
+    check('Marca').notEmpty().isLength({max:45}).withMessage("Invalid Marca"),
+    check('ano').notEmpty().isNumeric({max:20}).withMessage("Invalid ano"),
+    check('precio').notEmpty().isLength({max:30}).withMessage("Invalid precio"),
+    check('descripcion').notEmpty().isLength({max:100}).withMessage("Invalid descripcion"),
 ]
 
 const generateIdValidators = () => [
@@ -13,10 +13,10 @@ const generateIdValidators = () => [
 
 const updateautoValidators = () => [
     check('id').notEmpty().isNumeric().withMessage("Invalid id"),
-    check('alias').notEmpty().isLength({max:50}).withMessage("Invalid alias"),
-    check('type').notEmpty().isIn(['DOG','CAT']).withMessage("Invalid pet"),
-    check('color').notEmpty().isLength({max:50}).withMessage("Invalid color"),
-    check('notes').notEmpty().isLength({max:150}).withMessage("Invalid notes"),
+    check('Marca').notEmpty().isLength({max:50}).withMessage("Invalid alias"),
+    check('ano').notEmpty().isLength({max:20}).withMessage("Invalid auto"),
+    check('precio').notEmpty().isLength({max:30}).withMessage("Invalid predcio"),
+    check('discrepcion').notEmpty().isLength({max:100}).withMessage("Invalid discrepcion"),
 ]
 
 const reporter = (req,res,next) => {
