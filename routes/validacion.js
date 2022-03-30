aabe055a6a40c58689e3b3966b3efa059673913f
fernandosolis448 <luis.solis.is@unipolidgo.edu.mx>
2022-03-29 23:36:22 -0600

    Update validacion.js

--- routes/validacion.js
+++ routes/validacion.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const validacionsController = require('../controllers/adoptionsController');
-const adoptionValidator = require("../validations/adoptionValidator");
+const validacionsController = require('../controllers/validacionsController');
+const validacionValidator = require("../validations/validacionValidator");
 router.get('/validacion',validacionValidator.id, validacionsController.getvalidacion);
 router.get('/validacions', validacionsController.getvalidacions);
 router.post('/validacion',validacionValidator.add, validacionsController.postvalidacion);
 router.put('/validacion',validacionValidator.update, validacionsController.putvalidacion);
 router.delete('/validacion',validacionValidator.id, validacionsController.deletevalidacion);
-router.get('/validacion', validacionsController.getvalidacionByUser);
+router.get('/validacion', validacionsController.getvalidacionBycliente);
 
 module.exports = router;
