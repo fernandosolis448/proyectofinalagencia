196a30988c0d37c859686069b37034f27b3dff0c
fernandosolis448 <luis.solis.is@unipolidgo.edu.mx>
2022-03-31 08:15:59 -0600

    Update cliente.js

--- routes/cliente.js
+++ routes/cliente.js
@@ -4,10 +4,10 @@ const clientesController = require('../controllers/clientesController');
 const clienteValidator = require('../validations/clienteValidator');
 const jwtToken = require("../validations/jwtValidation");
 
-router.get('/cliente',jwtToken.ValidateToken ,clienteValidator.id, clientesController.getcliente);
-router.get('/clientes',jwtToken.ValidateToken  ,clientesController.getclientes);
-router.post('/cliente', clienteValidator.add, clientesController.postcliente);
-router.post('/login',clienteValidator.id, clientesController.getLogin);
+router.get('/cliente',clientesController.getcliente);
+router.get('/clientes', jwtToken.ValidateToken, clientesController.getclientes);
+router.post('/cliente' ,clienteValidator.add, clientesController.postcliente);
+router.post('/login', clienteValidator.id,clientesController.getLogin);
 router.put('/cliente', clienteValidator.update, clientesController.putcliente);
 router.delete('/cliente',clienteValidator.id , clientesController.deletecliente);
 
