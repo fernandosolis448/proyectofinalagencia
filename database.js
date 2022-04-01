f9c9c76a3a335b242faad34bddbc350a935a0402
fernandosolis448 <luis.solis.is@unipolidgo.edu.mx>
2022-03-31 18:13:27 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,10 +1,9 @@
 let config = {
-	host: "us-cdbr-east-05.cleardb.net",
-	user: "ba544c575b991d",
-	password: "c91b93d4",
+	host: "localhost",
+	user: "root",
+	password: "",
 	port: 3306,
-	database: "heroku_b259406693bc4d7",
+	database: "agencia",
 	secret_key:"cGFzc3dvcmQ="
 };
 exports.config = config;
-
