319b7cf78d30e0a9a4299f29595de78849174ae1
fernandosolis448 <luis.solis.is@unipolidgo.edu.mx>
2022-03-31 18:23:57 -0600

    Update cliente.js

--- routes/cliente.js
+++ routes/cliente.js
@@ -4,11 +4,11 @@ const clientesController = require('../controllers/clientesController');
 const clienteValidator = require('../validations/clienteValidator');
 const jwtToken = require("../validations/jwtValidation");
 
-router.get('/cliente',clientesController.getcliente);
+router.get('/cliente', jwtToken.ValidateToken, clientesController.getcliente);
 router.get('/clientes', jwtToken.ValidateToken, clientesController.getclientes);
-router.post('/cliente' ,clienteValidator.add, clientesController.postcliente);
-router.post('/login', clienteValidator.id,clientesController.getLogin);
-router.put('/cliente', clienteValidator.update, clientesController.putcliente);
-router.delete('/cliente',clienteValidator.id , clientesController.deletecliente);
+router.post('/cliente' , jwtToken.ValidateToken, clienteValidator.add, clientesController.postcliente);
+router.post('/login', jwtToken.ValidateToken, clienteValidator.id,clientesController.getLogin);
+router.put('/cliente', jwtToken.ValidateToken, clienteValidator.update, clientesController.putcliente);
+router.delete('/cliente', jwtToken.ValidateToken, clienteValidator.id , clientesController.deletecliente);
 
 module.exports = router;
