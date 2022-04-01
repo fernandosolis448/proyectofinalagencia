8550eb1c140b86973f1add2a2f4afeede6d1b423
fernandosolis448 <luis.solis.is@unipolidgo.edu.mx>
2022-03-31 18:35:51 -0600

    Update auto.js

--- routes/auto.js
+++ routes/auto.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const autosController = require('../controllers/autosController');
 const autoValidator = require("../validations/autoValidator");
-router.get('/auto',autosController.getauto);
-router.get('/autos', autosController.getautos);
+const jwtToken = require("../validations/jwtValidation");
+
+router.get('/auto',jwtToken.ValidateToken, autosController.getauto);
+router.get('/autos', jwtToken.ValidateToken,autosController.getautos);
 router.post('/auto',autoValidator.add, autosController.postauto);
 router.put('/auto',autoValidator.update, autosController.putauto);
 router.delete('/auto',autoValidator.id, autosController.deleteauto);
